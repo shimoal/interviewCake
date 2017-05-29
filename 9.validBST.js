e7d961ddf3aeb2f2482501fed9696b5126a02646
Alison Reed <alison.michelle.reed@gmail.com>
2017-05-28 19:45:09 -0700

    FInish recursive version of validBST

--- 9.validBST.js
+++ 9.validBST.js
@@ -1,5 +1,24 @@
-function validBST() {
-  
+// Write a function to check that a binary tree↴ is a
+// valid binary search tree↴ .
+
+// We can do this in O(n) time and O(n) additional space, where n is the number of nodes in our tree. Our additional space is O(lg n) if our tree is balanced.
+
+
+function validBST(tree, lowerBound, upperBound) {
+
+  lowerBound = lowerBound || -Infinity;
+  upperBound = upperBound || Infinity;
+
+    if (!tree) {
+      return true;
+    }
+
+    if (tree.val > upperBound || tree.val < lowerBound) {
+      return false;
+    }
+    
+    return validBST(tree.left, lowerBound, tree.val) && validBST(tree.right, tree.val, upperBound);
+
 }
 
 module.exports = validBST;
--- test/9.validBSTTest.js
+++ test/9.validBSTTest.js
@@ -3,8 +3,22 @@ var expect = chai.expect;
 var BST = require('../BST.js');
 var validBST = require('../9.validBST.js');
 
+
 describe('validBST', function() {
   it ('should be a function', function() {
     expect(validBST).to.be.a('function');
   });
+
+  it('should return false for balanced Invalid BST', function() {
+    var balancedInvalidBST = new BST(50);
+    balancedInvalidBST.addNode(30);
+    balancedInvalidBST.left.right = new BST(60);
+    balancedInvalidBST.addNode(80);
+    balancedInvalidBST.addNode(20);
+    balancedInvalidBST.addNode(70);
+    balancedInvalidBST.addNode(90);
+    
+    expect(validBST(balancedInvalidBST)).to.equal(false);
+  });
+
 });
