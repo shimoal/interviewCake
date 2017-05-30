f791f190c49fe8bfd5c82e4711626a431ae26ff5
Alison Reed <alison.michelle.reed@gmail.com>
2017-05-30 14:20:38 -0700

    Pass all tests for  stack with two qs

--- 20.stackWithTwoQs.js
+++ 20.stackWithTwoQs.js
@@ -1,5 +1,40 @@
 var Stack = function() {
-  
+  this.storage = {};
+  this.size = 0;
 }
 
-module.exports = Stack;
+Stack.prototype.push = function(val) {
+  this.size++;
+  this.storage[this.size] = val;
+}
+
+Stack.prototype.pop = function() {
+  var popped = this.storage[this.size];
+  delete this.storage[this.size];
+  this.size--;
+  return popped;
+}
+
+var Queue = function() {
+  this.inBox = new Stack();
+  this.outBox = new Stack();
+  this.size = 0;
+}
+
+Queue.prototype.enqueue = function(val) {
+  this.inBox.push(val);
+  this.size++;
+}
+
+Queue.prototype.dequeue = function() {
+  if (this.outBox.size === 0) {
+    while (this.inBox.size > 0) {
+      this.outBox.push(this.inBox.pop());
+    }
+  }
+
+  this.size--;
+  return this.outBox.pop();
+}
+
+module.exports = Queue;
--- test/20.stackWithTwoQsTest.js
+++ test/20.stackWithTwoQsTest.js
@@ -1,9 +1,25 @@
 var chai = require('chai');
 var expect = chai.expect;
-var Stack = require('../20.stackWithTwoQs.js');
+var Queue = require('../20.stackWithTwoQs.js');
 
 describe('20.stackWithTwoQs', function() {
   it('should be a function', function() {
-    expect(Stack).to.be.a('function');
+    expect(Queue).to.be.a('function');
+  });
+
+  it('should enqueue values', function() {
+    var q = new Queue();
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    expect(q.size).to.equal(3);
+  });
+
+  it('should dequeue values in order', function(){
+    var q = new Queue();
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    expect(q.dequeue()).to.equal(1);
   });
 });
