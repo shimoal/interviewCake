db57b2b7c1a7c424ce17ae8a6fb665a8cf90961b
Alison Reed <alison.michelle.reed@gmail.com>
2017-05-30 13:52:30 -0700

    Pass basic test for cakeThief

--- test/16.cakeThief.js
+++ test/16.cakeThief.js
@@ -6,4 +6,9 @@ describe('16. cakeThief', function() {
   it('should be a function', function() {
     expect(cakeThief).to.be.a('function');
   });
+
+  it('should find the optimal price for cakes', function() {
+    expect(cakeThief([[7, 160], [3, 90], [2, 15]], 20)).to.equal(555);
+    expect(cakeThief([[2, 15], [7, 160], [3, 90]], 20)).to.equal(555);
+  })
 });
