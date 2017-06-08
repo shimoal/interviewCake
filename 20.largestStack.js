3ddf51eea5db6d0baadb23566015c2f3db7e5bbb
Alison Reed <alison.michelle.reed@gmail.com>
2017-06-08 01:23:54 -0700

    Implement MaxStack in constant time and linear space

--- 20.largestStack.js
+++ 20.largestStack.js
@@ -1,5 +1,83 @@
-function largestStack() {
+// You've implemented a Stack class, but you want to be able to access the largest element in a stack.
+
+//Current Stack class:
+// class Stack:
+// # intialize an empty list def __init__(self):
+// self.items = []
+// # push a new item to the last index def push(self, item):
+// self.items.append(item)
+// # remove the last item def pop(self):
+// # if the stack is empty, return None
+// # (it would also be reasonable to throw an exception) if not self.items: return None
+// return self.items.pop()
+// # see what the last item is def peek(self):
+// # if the stack is empty, return None if not self.items: return None
+// return self.items[len(self.items)-1]
+// Use your Stack class to implement a new class MaxStack with a function get_max() that returns the largest element in the stack. get_max() should not remove the item.
+
+var Stack = function() {
+  this.items = [];
+}
+
+Stack.prototype.constructor = Stack;
+
+Stack.prototype.append = function(item) {
+  this.items.push(item);
+}
+
+Stack.prototype.pop = function() {
+  if (this.items.length > 0) {
+    return this.items.pop();
+  }
+  return 'None';
+}
+
+Stack.prototype.peek = function(item) {
+  if (this.items.length > 0) {
+    return this.items[this.items.length - 1];
+  } 
+  return 'None';
+}
+
+
+var MaxStack = function() {
+  this.items = [];
+  this.maxStack = [];
+  this.max = null
+}
+
+MaxStack.prototype.constructor = MaxStack;
+
+MaxStack.prototype.append = function(item) {
+  if (!this.max || this.max < item) {
+    this.max = item;
+  }
+
+  this.items.push(item);
+  this.maxStack.push(this.max);
+}
+
+MaxStack.prototype.pop = function() {
+  if (this.items.length === 0) {
+    return 'None';
+  }
+  this.maxStack.pop();
+  return this.items.pop();
+}
+
+MaxStack.prototype.getMax = function() {
+  if (this.maxStack.length > 0) {
+    return this.maxStack[this.maxStack.length - 1];    
+  }
+  return 'None';
 
 }
 
-module.exports = largestStack;
+MaxStack.prototype.peek = function() {
+  if (this.items.length > 0) {
+    return this.items[this.items.length - 1];
+  }
+  return 'None';
+}
+
+module.exports = MaxStack;
--- test/20.largestStackTest.js
+++ test/20.largestStackTest.js
@@ -1,9 +1,48 @@
 var chai = require('chai');
 var expect = chai.expect;
-var largestStack = require('../20.largestStack.js');
+var MaxStack = require('../20.largestStack.js');
 
 describe('20.largestStack', function() {
   it('should be a function', function() {
-    expect(largestStack).to.be.a('function');
+    expect(MaxStack).to.be.a('function');
   });
+
+  it('should add items to the stack', function() {
+    var max = new MaxStack();
+    max.append(1);
+    expect(max.items.length).to.equal(1);
+    max.append(2);
+    max.append(3);
+    max.append(4);
+    expect(max.items.length).to.equal(4);
+  });
+
+  it('should remove items', function() {
+    var max = new MaxStack();
+    max.append(10);
+    max.append(20);
+    max.append(50);
+    expect(max.pop()).to.equal(50);
+  });
+
+  it('should peek at the top of the stack', function() {
+    var max = new MaxStack();
+    max.append(10);
+    max.append(9);
+    max.append(8);
+    max.append(7);
+    max.append(6);
+    max.append(5);
+    expect(max.peek()).to.equal(5);
+    expect(max.items.length).to.equal(6);
+  });
+
+  it('should get the max of the stack without removing items', function() {
+    var max = new MaxStack();
+    max.append(10);
+    max.append(40);
+    max.append(3);
+    expect(max.getMax()).to.equal(40);
+    expect(max.items.length).to.equal(3);
+  })
 });
