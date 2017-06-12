5b8b9ae9307c84cb759a5dbab09b5c49bd25245a
Alison Reed <alison.michelle.reed@gmail.com>
2017-06-12 19:49:56 -0400

    Pass test for no cycle list

--- 23.linkedListCycle.js
+++ 23.linkedListCycle.js
@@ -1,5 +1,28 @@
-function hasCycle(linkedList) {
+//1, 2, 3, 4, 5
 
+function hasCycle(headOfList) {
+  var fastRunner = headOfList; 
+  var slowRunner = headOfList; 
+
+  var cycleFound = false; 
+
+  while (!cycleFound ) {
+
+    if (fastRunner.next === null || fastRunner.next.next === null ) {
+      return false;
+    }
+
+    
+    fastRunner = fastRunner.next.next;
+    slowRunner = slowRunner.next;
+
+    if (fastRunner === slowRunner) {
+      cycleFound = true;
+    }
+
+  }
+
+  return cycleFound;
 }
 
 module.exports = hasCycle;
--- test/23.linkedListCycleTest.js
+++ test/23.linkedListCycleTest.js
@@ -7,4 +7,14 @@ describe('23.hasCycle', function() {
   it('should be a function', function() {
     expect(hasCycle).to.be.a('function');
   });
+
+  it('should return false for LinkedList without a cycle', function() {
+    var list = new LinkedList();
+    var start = list.addNodeToTail(1);
+    list.addNodeToTail(2);
+    list.addNodeToTail(3);
+    list.addNodeToTail(4);
+    list.addNodeToTail(5);
+    expect(hasCycle(start)).to.equal(false);
+  })
 });
