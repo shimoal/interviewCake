186664439379b5d27b06d776448ccb32ad7bec1a
Alison Reed <alison.michelle.reed@gmail.com>
2017-06-12 19:53:06 -0400

    Pass all tests for has cycle

--- test/23.linkedListCycleTest.js
+++ test/23.linkedListCycleTest.js
@@ -16,5 +16,27 @@ describe('23.hasCycle', function() {
     list.addNodeToTail(4);
     list.addNodeToTail(5);
     expect(hasCycle(start)).to.equal(false);
-  })
+  });
+
+  it('should return true for LinkedList with a cycle in the middle', function() {
+    var list = new LinkedList();
+    var start = list.addNodeToTail(1);
+    var cyclePoint = list.addNodeToTail(2);
+    list.addNodeToTail(3);
+    list.addNodeToTail(4);
+    var cycle = list.addNodeToTail(5);
+    cycle.next = cyclePoint;
+    expect(hasCycle(start)).to.equal(true);
+  });
+
+  it('should return true for LinkedList with a cycle going back to the head', function() {
+    var list = new LinkedList();
+    var start = list.addNodeToTail(1);
+    list.addNodeToTail(2);
+    list.addNodeToTail(3);
+    list.addNodeToTail(4);
+    var cycle = list.addNodeToTail(5);
+    cycle.next = start;
+    expect(hasCycle(start)).to.equal(true);    
+  });
 });
