afa0caea21b305826c2c2ee5ece41b61141759e8
Alison Reed <alison.michelle.reed@gmail.com>
2017-06-12 20:01:22 -0400

    Write first test for reverseList

--- test/24.reverseLinkedListTest.js
+++ test/24.reverseLinkedListTest.js
@@ -1,3 +1,10 @@
 var chai = require('chai');
 var expect = chai.expect;
 var LinkedList = require('../LinkedList.js');
+var reverseList = require('../24.reverseLinkedList.js');
+
+describe('reverseList', function() {
+  it ('should be a function', function() {
+    expect(reverseList).to.be.a('function');
+  });
+});
