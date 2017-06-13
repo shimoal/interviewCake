f1c2864c1c8702d899c6726553399ceba4a54627
Alison Reed <alison.michelle.reed@gmail.com>
2017-06-12 20:17:04 -0400

    Reverse a linked List in place

--- 24.reverseLinkedList.js
+++ 24.reverseLinkedList.js
@@ -1,4 +1,21 @@
-function reverseLinkedList() {
+function reverseLinkedList(head) {
+  if (head === null) {
+    return null
+  }
+
+  var previous = null;
+  var current = head;
+
+  while (current.next) {
+    var oldNext = current.next;
+    current.next = previous;
+    previous = current;
+    current = oldNext;
+  }
+
+  current.next = previous;
+  return current;
+
 
 }
 
--- test/24.reverseLinkedListTest.js
+++ test/24.reverseLinkedListTest.js
@@ -7,4 +7,31 @@ describe('reverseList', function() {
   it ('should be a function', function() {
     expect(reverseList).to.be.a('function');
   });
+
+  it('should return the previous tail as the new head', function() {
+    var list = new LinkedList();
+    var a = list.addNodeToTail('a');
+    var b = list.addNodeToTail('b');
+    var c = list.addNodeToTail('c');
+    var d = list.addNodeToTail('d');
+    var reverseHead = reverseList(a);
+    expect(reverseHead).to.equal(d);
+  });
+
+  it('should properly link to the nodes in reverse order', function() {
+    var list = new LinkedList();
+    var a = list.addNodeToTail('a');
+    var b = list.addNodeToTail('b');
+    var c = list.addNodeToTail('c');
+    var d = list.addNodeToTail('d');
+    var reverseHead = reverseList(a);
+    expect(reverseHead.next).to.equal(c);
+    expect(reverseHead.next.next).to.equal(b);
+    expect(reverseHead.next.next.next).to.equal(a);
+  });
+
+  it('should work on an empty list', function() {
+    var list = new LinkedList();
+    expect(reverseList(list.head)).to.equal(null);
+  })
 });
