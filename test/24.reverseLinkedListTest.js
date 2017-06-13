2c0c042b07b75dd7c6d132fe93a630412a080d9e
Alison Reed <alison.michelle.reed@gmail.com>
2017-06-12 20:19:33 -0400

    Add problem number to test

--- test/24.reverseLinkedListTest.js
+++ test/24.reverseLinkedListTest.js
@@ -3,7 +3,7 @@ var expect = chai.expect;
 var LinkedList = require('../LinkedList.js');
 var reverseList = require('../24.reverseLinkedList.js');
 
-describe('reverseList', function() {
+describe('24.reverseList', function() {
   it ('should be a function', function() {
     expect(reverseList).to.be.a('function');
   });
