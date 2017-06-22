a68e5f78c2c77fb00839f75bf3d8a51c2db6d707
Alison Reed <alison.michelle.reed@gmail.com>
2017-06-22 12:35:56 -0400

    Refactor problem 31 to use an immedate recursive function instead of a helper

--- 31.stringPermutations.js
+++ 31.stringPermutations.js
@@ -1,24 +1,43 @@
-function stringPermutations(string) {
-  var permutations = [];
+// function stringPermutations(string) {
+//   var permutations = [];
 
-  var findAllPermutations = function(charsLeft, currentPerm) {
+//   var findAllPermutations = function(charsLeft, currentPerm) {
+//     currentPerm = currentPerm || '';
+
+//     if (charsLeft.length === 1) {
+//       currentPerm += charsLeft.charAt(0);
+//       permutations.push(currentPerm);
+//       return;
+//     }
+
+//     for (var i = 0; i < charsLeft.length; i++) {
+//       var newCharsLeft = charsLeft.slice(0, i) + charsLeft.slice(i + 1);
+//       findAllPermutations(newCharsLeft, currentPerm + charsLeft.charAt(i));
+//     }
+
+//   };
+
+//   findAllPermutations(string);
+//   return permutations;
+// };
+
+function stringPermutations(charsLeft, currentPerm, allPerms) {
     currentPerm = currentPerm || '';
+    allPerms = allPerms || [];
 
     if (charsLeft.length === 1) {
       currentPerm += charsLeft.charAt(0);
-      permutations.push(currentPerm);
-      return;
+      allPerms.push(currentPerm);
+      return allPerms;
     }
 
     for (var i = 0; i < charsLeft.length; i++) {
       var newCharsLeft = charsLeft.slice(0, i) + charsLeft.slice(i + 1);
-      findAllPermutations(newCharsLeft, currentPerm + charsLeft.charAt(i));
+      stringPermutations(newCharsLeft, currentPerm + charsLeft.charAt(i), allPerms);
     }
 
-  };
+    return allPerms;
 
-  findAllPermutations(string);
-  return permutations;
 };
 
 module.exports = stringPermutations;
