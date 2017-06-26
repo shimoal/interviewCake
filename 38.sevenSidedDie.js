f00f51946b7229ee637e06075ba7210108f05f9f
Alison Reed <alison.michelle.reed@gmail.com>
2017-06-26 17:40:27 -0400

    Pass tests for 38 - sevenSidedDie

--- 38.sevenSidedDie.js
+++ 38.sevenSidedDie.js
@@ -1,5 +1,26 @@
-function rand7() {
+function rand5() {
+  return Math.floor(Math.random() * 5) + 1;
+}
 
+function rand7() {
+  var results = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 1, 2, 3],
+    [4, 5, 6, 7, 1],
+    [2, 3, 4, 5, 6],
+    [7, 0, 0, 0, 0]
+  ];
+
+
+  var row = rand5() - 1;
+  var col = rand5() - 1;
+
+  while (row === 4 && col > 0) {
+    row = rand5() - 1;
+    col = rand5() - 1;
+  }
+
+  return results[row][col];
 }
 
 module.exports = rand7;
--- test/38.sevenSidedDieTest.js
+++ test/38.sevenSidedDieTest.js
@@ -10,12 +10,13 @@ describe('38.rand7', function() {
   it('should return a different random number each call', function() {
     var rand1 = rand7();
     var rand2 = rand7();
+    //will fail 1/7 times
     expect(rand1).to.not.equal(rand2);
   });
 
-  it('should return a number between 1 and 5', function() {
+  it('should return a number between 1 and 7', function() {
     expect(rand7()).to.be.at.least(1);
-    expect(rand7()).to.be.below(6);
+    expect(rand7()).to.be.below(8);
   });
 
   it('should return a number', function() {
