3cdafc1de91d6f85d7dee8bf6ba17fc9d2829d09
Alison Reed <alison.michelle.reed@gmail.com>
2017-06-25 21:59:24 -0400

    Finish problem 36

--- 36.singleRiffleShuffle.js
+++ 36.singleRiffleShuffle.js
@@ -1,5 +1,27 @@
-function singleRiffleShuffle(array) {
-  return array;
+function singleRiffleShuffle(shuffledDeck, half1, half2) {
+
+  half1Index = 0;
+  half2Index = 0;
+
+  for (var shuffledDeckIndex = 0; shuffledDeckIndex < shuffledDeck.length; shuffledDeckIndex++) {
+
+    if (half1[half1Index] &&
+      half1[half1Index] === shuffledDeck[shuffledDeckIndex]) {
+      half1Index++;
+    } else
+
+    if (half2[half2Index] &&
+      half2[half2Index] === shuffledDeck[shuffledDeckIndex]) {
+      half2Index++;
+    } 
+
+
+
+    else {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = singleRiffleShuffle;
--- test/36.singleRiffleShuffleTest.js
+++ test/36.singleRiffleShuffleTest.js
@@ -7,7 +7,11 @@ describe('36.singleRiffleShuffle', function() {
     expect(singleRiffleShuffle).to.be.a('function');
   });
 
-  it('should shuffle an array', function() {
-    expect(singleRiffleShuffle([1, 2, 3, 4, 5])).to.not.deep.equal([1, 2, 3, 4, 5]);
+  it('should detect a shuffle of a single riffle', function() {
+    expect(singleRiffleShuffle([1, 2, 3, 4, 5], [1, 2, 3], [4, 5])).to.equal(true);
+  });
+
+  it('should return false if a deck must have been shuffled more than a single riffle', function() {
+    expect(singleRiffleShuffle([1, 3, 5, 6, 2], [1, 5, 3], [2, 6])).to.equal(false);
   })
 });
