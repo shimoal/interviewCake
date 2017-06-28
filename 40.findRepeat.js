5c15cfca02e1be8664e944153be6743cca79bb7d
Alison Reed <alison.michelle.reed@gmail.com>
2017-06-28 12:24:28 -0700

    Pass initial test for problem 40

--- 40.findRepeat.js
+++ 40.findRepeat.js
@@ -1,5 +1,13 @@
-function findRepeat() {
-
+function findRepeat(array) {
+  var sortedArray = array.sort(function(a, b) {
+    return a - b;
+  });
+  for (var i = 0; i < sortedArray.length - 1; i++) {
+    if (sortedArray[i] === sortedArray[i+1]) {
+      return sortedArray[i];
+    }
+  }
+  return -1;
 }
 
 module.exports = findRepeat;
--- test/40.findRepeatTest.js
+++ test/40.findRepeatTest.js
@@ -6,4 +6,8 @@ describe('40.findRepeat', function() {
   it('should be a function', function() {
     expect(findRepeat).to.be.a('function');
   });
+
+  it('should find any integer that is a duplicate', function() {
+    expect(findRepeat([2, 3, 4, 5, 1, 4])).to.equal(4);
+  })
 });
