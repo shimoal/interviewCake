597a91d29b17bd87b5e61d7ee4b343c1de07e14a
Alison Reed <alison.michelle.reed@gmail.com>
2017-06-28 12:58:46 -0700

    Pass test for problem 43

--- 43.mergeSortedArrays.js
+++ 43.mergeSortedArrays.js
@@ -1,4 +1,19 @@
 function mergeArrays(array1, array2) {
+  var mergedArray = [];
+  var index1 = 0;
+  var index2 = 0;
+
+  for (var mergedIndex = 0; mergedIndex < array1.length + array2.length; mergedIndex++) {
+    if (!array2[index2] || array1[index1] <= array2[index2]) {
+      mergedArray.push(array1[index1]);
+      index1++;
+    } else {
+      mergedArray.push(array2[index2]);
+      index2++;
+    }
+  }
+
+  return mergedArray;
 
 }
 
--- test/43.mergeSortedArraysTest.js
+++ test/43.mergeSortedArraysTest.js
@@ -6,4 +6,12 @@ describe('43.mergeArrays', function() {
   it('should be a function', function() {
     expect(mergeArrays).to.be.a('function');
   });
+
+  it('should return an array', function() {
+    expect(mergeArrays([1], [1])).to.be.an('array');
+  });
+
+  it('should return a sorted array', function() {
+    expect(mergeArrays([1, 5, 9, 10], [2, 3, 4, 6, 7, 8])).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
 });
