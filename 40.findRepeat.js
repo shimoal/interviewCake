e4148c92750c711872d5abca6bbcfe9a75684b9e
Alison Reed <alison.michelle.reed@gmail.com>
2017-06-28 12:18:04 -0700

    Add files for problem 40

---
+++ 40.findRepeat.js
@@ -0,0 +1,5 @@
+function findRepeat() {
+
+}
+
+module.exports = findRepeat;
---
+++ test/40.findRepeatTest.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
+var findRepeat = require('../40.findRepeat');
+
+describe('40.findRepeat', function() {
+  it('should be a function', function() {
+    expect(findRepeat).to.be.a('function');
+  });
+});
